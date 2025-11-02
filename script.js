8916abaeb1f5575a1d5f0c62b4e95950d87147f8
ankitgujre <ankitgujre374@gmail.com>
2025-11-02 23:55:26 +0530

    switch case

---
+++ script.js
@@ -0,0 +1,13 @@
+switch(4){
+    case 1:
+        console.log("Hello1")
+        break
+    case 2:
+        console.log("Hello2")
+        break
+    case 3:
+        console.log("Hello 3")
+        break
+    default:
+        console.log("Deagault case")            
+}
