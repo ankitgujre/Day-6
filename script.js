f8e5ff44fb280febc6f02affde27c11862088d14
ankitgujre <ankitgujre374@gmail.com>
2025-11-08 19:08:00 +0530

    reverse number

--- script.js
+++ script.js
@@ -18,16 +18,40 @@ switch(4){
 
 // }
 
-for(let i = 0; i<5; i++){
-    console.log("Ankit")
-}
+// for(let i = 0; i<5; i++){
+//     console.log("Ankit")
+// }
 
-for (let i = 11; i < 16; i++){
-    console.log(i)
-}
+// for (let i = 11; i < 16; i++){
+//     console.log(i)
+// }
 
-console.log("Break")
+// console.log("Break")
 
-for (let i = 30; i > 9; i--){
+// for (let i = 30; i > 9; i--){
+//     console.log(i)
+// }
+
+// for(let i = 1; i <=10; i++){
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 20; i++){
+//     if(i%2===0){
+//         console.log(i + "= even")
+//     }else{
+//         console.log("odd")
+//     }
+// }
+
+// for(let i = 1; i <= 20; i++){
+//     if(i/2===0){
+//         console.log(i + "= even")
+//     }else{
+//         console.log("odd")
+//     }
+// }
+
+for(let i = 10; i >= 1; i--){
     console.log(i)
 }
